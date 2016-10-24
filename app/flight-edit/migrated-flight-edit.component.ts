f0c2d21d80d9b0d80bb9dfbb1dc9b9a442e70f90
Carlos Olivares <carlos.olivares.ve@gmail.com>
2016-10-24 17:30:44 +0200

    Fixing ngInit

--- app/flight-edit/migrated-flight-edit.component.ts
+++ app/flight-edit/migrated-flight-edit.component.ts
@@ -1,33 +1,39 @@
 /**
  * Created by colivares on 10/24/16.
  */
+import * as angular from 'angular';
 import {FlightService} from '../services/flight.service';
 import {Flight} from "../shared/flight";
-import {Component, OnInit, Input, Inject} from '@angular/core';
+
+import { Component, Input, OnInit, Inject } from '@angular/core';
 
 @Component({
     selector: 'flight-edit',
     templateUrl: './migrated-flight-edit.component.html'
 })
-
-export class MigratedFlightEditComponent {
-    @Input() id: string;
-    flight: Flight = <any>{};
+export class MigratedFlightEditComponent implements OnInit {
     info = 'Flight Edit';
+
+    @Input() id: string;
+
+    flight: Flight = <any>{};
     message: string;
 
-    constructor(@Inject('flightService') private flightService: FlightService) {
+    constructor(
+        @Inject('flightService') private flightService: FlightService) {
+    }
 
-            this.flightService
-                .getById(this.id)
-                .then((flight: Flight) => {
-                    this.flight = flight;
-                    this.message = "";
-                })
-                .catch(resp => {
-                    console.error(resp);
-                    this.message = resp.data;
-                })
+    ngOnInit() {
+        this.flightService
+            .getById(this.id)
+            .then((flight: Flight) => {
+                this.flight = flight;
+                this.message = '';
+            })
+            .catch(resp => {
+                console.error(resp);
+                this.message = resp.data;
+            })
 
     }
 
